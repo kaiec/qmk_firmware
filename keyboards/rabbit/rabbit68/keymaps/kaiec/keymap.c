c5cc5d7b0bc240ccb50aeab52b09f8dbefb5ad43
Kai Eckert <hallo@kaiec.de>
2019-09-11 13:38:03 +0200

    Mouse Keys

--- keyboards/rabbit/rabbit68/keymaps/kaiec/keymap.c
+++ keyboards/rabbit/rabbit68/keymaps/kaiec/keymap.c
@@ -28,13 +28,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_NO,            KC_SPC,  KC_SPC,           KC_RALT, MO(_FN), KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT
+  KC_LCTL, KC_LGUI, KC_LALT, KC_NO,            KC_SPC,  KC_SPC,           KC_RALT, MO(_FN), KC_LCTL, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
     [_FN] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______,
-  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______, _______, _______ 
+  _______, KC_F1,      KC_F2,      KC_F3,       KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,
+  _______, KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  KC_MS_BTN3, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______,    _______,    _______,     _______,    _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______,
+  _______, _______,    _______,    _______,     _______,    _______,          _______, _______, _______, _______, _______, _______ 
   ),
 };
